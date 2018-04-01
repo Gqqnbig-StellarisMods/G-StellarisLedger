4030e0699ae2c3a063fd7df76e941fda65f8e8cd
gqqnbig <gqqnb2005@gmail.com>
2018-03-31 21:21:07 -0700

    用JSON输出所有数据

---
+++ StellarisInGameLedgerInCSharp/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Antlr4.Runtime.Tree;
+
+namespace StellarisInGameLedgerInCSharp
+{
+    public class Country
+    {
+        public string Tag { get; set; }
+        public string Name { get; internal set; }
+        public int TechnologyCount { get; set; }
+        public double MilitaryPower { get; set; }
+        public int ColonyCount { get; set; }
+        public int CivilianStations { get; internal set; }
+        public int Population { get; internal set; }
+        public double Energy { get; set; }
+        public double Minerals { get; set; }
+        public double Food { get; set; }
+        public double Influence { get; set; }
+        public double Unity { get; set; }
+        public double EnergyIncome { get; set; }
+        public double MineralsIncome { get; internal set; }
+        public double FoodIncome { get; internal set; }
+        public double InfluenceIncome { get; internal set; }
+        public double UnityIncome { get; internal set; }
+        public double PhysicsResearchIncome { get; internal set; }
+        public double SocietyResearchIncome { get; set; }
+        public double EngineeringResearchIncome { get; internal set; }
+    }
+}
---
+++ StellarisInGameLedgerInCSharp/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StellarisInGameLedgerInCSharp
+{
+    public static class StringExtensions
+    {
+        public static string DetectLineEnding(this string content)
+        {
+            string lineEndingSymbol;
+            if (content.Contains("\r\n"))
+                lineEndingSymbol = "\r\n";
+            else if (content.Contains("\r"))
+                lineEndingSymbol = "\r";
+            else
+                lineEndingSymbol = "\n";
+            return lineEndingSymbol;
+        }
+    }
+}
