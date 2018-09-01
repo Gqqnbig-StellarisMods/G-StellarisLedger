0e47dbeae892b38676c6b31845f03357a3230928
gqqnbig <gqqnb2005@gmail.com>
2018-08-31 22:46:44 -0700

    读取群星本地化文件，支持群星支持的所有语言

---
+++ StellarisInGameLedgerInCSharp/YamlStringLocalizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace StellarisInGameLedgerInCSharp
+{
+	public class YamlStringLocalizer : IStringLocalizer
+	{
+		public YamlStringLocalizer(IOptions<AppSettings> appSettings)
+		{
+			var c = CultureInfo.CurrentCulture;
+			//appSettings.Value.LocalizationModPath
+		}
+
+
+		public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IStringLocalizer WithCulture(CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public LocalizedString this[string name]
+		{
+			get
+			{
+				return new LocalizedString(name, "hello");
+			}
+		}
+
+		public LocalizedString this[string name, params object[] arguments] => throw new NotImplementedException();
+	}
+
+}
