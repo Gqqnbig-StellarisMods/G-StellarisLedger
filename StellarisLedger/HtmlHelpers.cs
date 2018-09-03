f75c1e72050580a5ff9b1d220ddbc68f0c0bc37d
gqqnbig <gqqnb2005@gmail.com>
2018-09-02 18:59:10 -0700

    增加更多本地化文本

---
+++ StellarisLedger/HtmlHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StellarisLedger
+{
+	public static class HtmlHelpers
+	{
+		public static IHtmlContent RawResource(this IHtmlHelper html, LocalizedHtmlString value)
+		{
+			return html.Raw(value.Value);
+		}
+
+		public static IHtmlContent Raw(this IHtmlHelper html, LocalizedHtmlString format, params LocalizedHtmlString[] objects)
+		{
+			return html.Raw(string.Format(format.Value, objects.Select(s => s.Value).ToArray()));
+		}
+	}
+}
