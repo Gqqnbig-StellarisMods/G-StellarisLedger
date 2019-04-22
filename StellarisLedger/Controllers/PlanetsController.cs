f511be9c6f45183821e4abce6f756503b1e23d75
gqqnbig <gqqnb2005@gmail.com>
2019-04-22 11:06:01 -0700

    创建planets页面

---
+++ StellarisLedger/Controllers/PlanetsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace StellarisLedger.Controllers
+{
+	public class PlanetsController : Controller
+	{
+		private string saveGamesPath;
+
+		public PlanetsController(IOptions<AppSettings> appSettings)
+		{
+			saveGamesPath = Path.GetFullPath(appSettings.Value.SaveGamesPath);
+		}
+
+		[HttpGet(@"latest/latest/Countries/me/Planets")]
+		public ActionResult Index()
+		{
+			var fileName= Stellaris.GetMostRecentSaves(saveGamesPath, 1).FirstOrDefault();
+			if (fileName == null)
+				return View();
+
+			var content = Stellaris.GetGameSaveContent(System.IO.Path.Combine(saveGamesPath, fileName));
+			var analyst=new Analyst(content);
+			var planetTitleses= analyst.GetCountryPlanetTiles("0");
+
+			return View(planetTitleses.ToArray());
+		}
+	}
+}
