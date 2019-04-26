044c6a05e6b54d4be5bc37310df6ced60165ca74
gqqnbig <gqqnb2005@gmail.com>
2019-04-26 15:27:51 -0700

    按照MVC建议，模型放在Models文件夹

---
+++ StellarisLedger/Models/Planets/PlanetsIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StellarisLedger.Models
+{
+	public class PlanetsIndexViewModel
+	{
+		public string LatestSaveGamesPath { get; set; }
+
+		public bool IsMachineEmpire { get; set; }
+	}
+}
