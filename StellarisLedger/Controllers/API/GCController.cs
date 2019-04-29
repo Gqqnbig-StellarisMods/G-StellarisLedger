5fcfd192dc4fb9ce575804bd06892f2b564e4779
gqqnbig <gqqnb2005@gmail.com>
2019-04-28 21:40:34 -0700

    在网页加载完毕后，调用GC

---
+++ StellarisLedger/Controllers/API/GCController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StellarisLedger.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/gc/")]
+    public class GCController : Controller
+    {
+		[HttpGet("run")]
+		public void Run()
+		{
+			var logger = ApplicationLogging.CreateLogger<GCController>();
+			logger.LogInformation("GC.Collect()");
+			GC.Collect();
+		}
+    }
+}
