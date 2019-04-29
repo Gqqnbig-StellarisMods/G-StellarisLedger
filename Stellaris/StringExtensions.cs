bb383dcd268b1c839688591b65e27cda49cba5ca
gqqnbig <gqqnb2005@gmail.com>
2019-04-28 19:24:32 -0700

    延迟解析Planets
    dotMemory探查显示，PlanetsData在解析时占用480M内存。因为我不会查询所有星球，所以把这段文本手动以字典的方式存起来，需要时再用Antlr解析。
    
    这样的话运行时总内存下降到2G，结束内存800MB，force gc 400MB。

--- Stellaris/StringExtensions.cs
+++ Stellaris/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace StellarisLedger
+﻿using System;
+using System.Text;
+
+namespace StellarisLedger
 {
     public static class StringExtensions
     {
@@ -13,5 +16,14 @@
                 lineEndingSymbol = "\n";
             return lineEndingSymbol;
         }
-    }
+
+		public static int IndexOf(this ReadOnlySpan<char> content, ReadOnlySpan<char> value, int startIndex)
+		{
+			var p = content.Slice(startIndex).IndexOf(value);
+			if (p == -1)
+				return -1;
+			else
+				return startIndex + p;
+		}
+	}
 }
