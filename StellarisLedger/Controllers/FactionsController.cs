7992fb2f9584b162f23b1c05e35a262eebbd2c44
gqqnbig <gqqnb2005@gmail.com>
2019-05-09 16:30:52 -0700

    重新实现了派系页面

--- StellarisLedger/Controllers/FactionsController.cs
+++ StellarisLedger/Controllers/FactionsController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 
 namespace StellarisLedger.Controllers
 {
 	[Route("Factions/")]
 	public class FactionsController : Controller
     {
+		private string saveGamesPath;
+
+		public FactionsController(IOptions<AppSettings> appSettings)
+		{
+			saveGamesPath = Path.GetFullPath(appSettings.Value.SaveGamesPath);
+		}
+
 		[HttpGet]
         // GET: Factions
         public ActionResult Index()
-        {
-            return View();
+		{
+			var fileName = Stellaris.GetMostRecentSaves(saveGamesPath, 1).FirstOrDefault();
+			ViewData["LatestSaveGamesPath"] = fileName;
+
+			return View();
         }
     }
 }
